940376f1d669a2e08d00cf6c3d72d748875448bb
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-03-24 22:53:17 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
-
-//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
+// import './App.css';
 
 // import "./App.css";
 // import { Configuration, OpenAIApi } from "openai";
@@ -12,79 +10,83 @@ import './App.css';
 //   const [response, setResponse] = useState("");
 //   const [loading, setLoading] = useState(false);
 
-//       const configuration = new Configuration({
-//         // apiKey: process.env.OPENAI_API_KEY,
-//         apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+//   const configuration = new Configuration({
+//     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+//   });
+//   const openai = new OpenAIApi(configuration);
+
+//   const HandleSubmit = (e) => {
+//     setLoading(true);
+//     e.preventDefault();
+//     openai
+//       .createCompletion({
+//         model: "text-davinci-002",
+//         prompt: text,
+//         temperature: 0.6,
+//         max_tokens: 100,
+//       })
+//       .then((res) => {
+//         if (res.status === 200) {
+//           setLoading(false);
+//           setResponse(res?.data?.choices[0]?.text);
+//         }
+//       })
+//       .catch((err) => {
+//         console.log(err, "An error occured");
 //       });
-//       const openai = new OpenAIApi(configuration);
+//   };
 
-//       const HandleSubmit = (e) => {
-//         setLoading(true);
-//         e.preventDefault();
-//         openai
-//           .createCompletion({
-//             model: "davinci",
-//             prompt: generatePrompt(text),
-//             temperature: 0.6,
-//             max_tokens: 100,
-//           })
-//           .then((res) => {
-//             if (res.status === 200) {
-//               setLoading(false);
-//               setResponse(res?.data?.choices[0]?.text);
-//             }
-//           })
-//           .catch((err) => {
-//             console.log(err, "An error occured");
-//           });
-//       };
+//   return (
+//     <div className="App_">
+//       <div className="header">
+//         <h1 className="header_text">
+//           Chat<span className="text_active">GPT</span>
+//         </h1>
+//         <h2 className="header_summary">Get responses from ChatGPT.</h2>
+//       </div>
+//       <div className="container">
+//         <div className="text_form">
+//           <form>
+//             <label>What kind </label>
+//             <textarea
+//               rows={14}
+//               cols={80}
+//               placeholder="Put your text"
+//               value={text}
+//               onChange={(e) => setText(e.target.value)}
+//             />
+//           </form>
+//         </div>
+//         <div>
+//           <button type="button" onClick={HandleSubmit}>
+//             {loading ? "loading..." : "Submit"}
+//           </button>
+//         </div>
+//         <div className="response_text">
+//           <label>Response from ChatGPT</label>
+//           <textarea
+//             placeholder="ChatGPT response"
+//             cols={80}
+//             rows={14}
+//             value={response}
+//             readOnly
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-//       function generatePrompt(text) {
-//         return `Generate a response to "${text}".`;
-//       }
+// export default App;
 
-//       return (
-//         <div className="container">
-//   <div className="text_form">
-//     <form>
-//       <label>Enter your text</label>
-//       <textarea
-//         rows={14}
-//         cols={80}
-//         placeholder="Put your text"
-//         value={text}
-//         onChange={(e) => setText(e.target.value)}
-//       />
-//     </form>
-//   </div>
-//   <div>
-//     <button type="button" onClick={HandleSubmit}>
-//       {loading ? "loading..." : "Get Response"}
-//     </button>
-//   </div>
-//   <div className="summarized_text">
-//     <label>ChatGPT's response</label>
-//     <textarea
-//       placeholder="ChatGPT's response"
-//       cols={80}
-//       rows={14}
-//       value={response}
-//       onChange={(e) => setText(e.target.value)}
-//     />
-//   </div>
-// </div>
-        
-//       );
-//     }
 
-//     export default App;
 
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("");
+  const [inputTexts, setInputTexts] = useState(["", "", ""]);
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -93,13 +95,14 @@ function App() {
   });
   const openai = new OpenAIApi(configuration);
 
-  const HandleSubmit = (e) => {
-    setLoading(true);
+  const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+    const prompt = inputTexts.join(" "); // concatenate input texts into a single string
     openai
       .createCompletion({
         model: "text-davinci-002",
-        prompt: text,
+        prompt,
         temperature: 0.6,
         max_tokens: 100,
       })
@@ -110,7 +113,7 @@ function App() {
         }
       })
       .catch((err) => {
-        console.log(err, "An error occured");
+        console.log(err, "An error occurred");
       });
   };
 
@@ -125,19 +128,43 @@ function App() {
       <div className="container">
         <div className="text_form">
           <form>
-            <label>Enter your text</label>
+            {/* Add multiple input textboxes */}
+            <label>Enter text:</label>
+            <br />
             <textarea
-              rows={14}
+              rows={3}
               cols={80}
-              placeholder="Put your text"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
+              placeholder="Describe what kind of "
+              value={inputTexts[0]}
+              onChange={(e) =>
+                setInputTexts([e.target.value, inputTexts[1], inputTexts[2]])
+              }
+            />
+            <br />
+            <textarea
+              rows={3}
+              cols={80}
+              placeholder="Text 2"
+              value={inputTexts[1]}
+              onChange={(e) =>
+                setInputTexts([inputTexts[0], e.target.value, inputTexts[2]])
+              }
+            />
+            <br />
+            <textarea
+              rows={3}
+              cols={80}
+              placeholder="Text 3"
+              value={inputTexts[2]}
+              onChange={(e) =>
+                setInputTexts([inputTexts[0], inputTexts[1], e.target.value])
+              }
             />
           </form>
         </div>
         <div>
-          <button type="button" onClick={HandleSubmit}>
-            {loading ? "loading..." : "Submit"}
+          <button type="button" onClick={handleSubmit}>
+            {loading ? "Loading..." : "Submit"}
           </button>
         </div>
         <div className="response_text">
