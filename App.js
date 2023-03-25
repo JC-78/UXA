e0afc2c616bee7895ffe8a9517d016c573b5d961
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-03-24 23:41:57 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-// import './App.css';
+//import logo from './logo.svg';
 
 // import "./App.css";
 // import { Configuration, OpenAIApi } from "openai";
 // import { useState } from "react";
 
 // function App() {
-//   const [text, setText] = useState("");
+//   const [inputTexts, setInputTexts] = useState(["", "", ""]);
 //   const [response, setResponse] = useState("");
 //   const [loading, setLoading] = useState(false);
 
@@ -15,13 +14,14 @@ import logo from './logo.svg';
 //   });
 //   const openai = new OpenAIApi(configuration);
 
-//   const HandleSubmit = (e) => {
-//     setLoading(true);
+//   const handleSubmit = (e) => {
 //     e.preventDefault();
+//     setLoading(true);
+//     const prompt = inputTexts.join(" "); // concatenate input texts into a single string
 //     openai
 //       .createCompletion({
 //         model: "text-davinci-002",
-//         prompt: text,
+//         prompt,
 //         temperature: 0.6,
 //         max_tokens: 100,
 //       })
@@ -32,7 +32,7 @@ import logo from './logo.svg';
 //         }
 //       })
 //       .catch((err) => {
-//         console.log(err, "An error occured");
+//         console.log(err, "An error occurred");
 //       });
 //   };
 
@@ -47,19 +47,43 @@ import logo from './logo.svg';
 //       <div className="container">
 //         <div className="text_form">
 //           <form>
-//             <label>What kind </label>
+//             {/* Add multiple input textboxes */}
+//             <label>Enter text:</label>
+//             <br />
 //             <textarea
-//               rows={14}
+//               rows={3}
 //               cols={80}
-//               placeholder="Put your text"
-//               value={text}
-//               onChange={(e) => setText(e.target.value)}
+//               placeholder="What kind of vibe are you going for with your colours?        "
+//               value={inputTexts[0]}
+//               onChange={(e) =>
+//                 setInputTexts([e.target.value, inputTexts[1], inputTexts[2]])
+//               }
+//             />
+//             <br />
+//             <textarea
+//               rows={3}
+//               cols={80}
+//               placeholder="Describe what you would want from your logo"
+//               value={inputTexts[1]}
+//               onChange={(e) =>
+//                 setInputTexts([inputTexts[0], e.target.value, inputTexts[2]])
+//               }
+//             />
+//             <br />
+//             <textarea
+//               rows={3}
+//               cols={80}
+//               placeholder="What kind of vibe/ambiance do you want with your text?              "
+//               value={inputTexts[2]}
+//               onChange={(e) =>
+//                 setInputTexts([inputTexts[0], inputTexts[1], e.target.value])
+//               }
 //             />
 //           </form>
 //         </div>
 //         <div>
-//           <button type="button" onClick={HandleSubmit}>
-//             {loading ? "loading..." : "Submit"}
+//           <button type="button" onClick={handleSubmit}>
+//             {loading ? "Loading..." : "Submit"}
 //           </button>
 //         </div>
 //         <div className="response_text">
@@ -78,16 +102,14 @@ import logo from './logo.svg';
 // }
 
 // export default App;
-
-
-
+import logo from "./logo.svg";
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 
 function App() {
   const [inputTexts, setInputTexts] = useState(["", "", ""]);
-  const [response, setResponse] = useState("");
+  const [responses, setResponses] = useState(["", "", ""]);
   const [loading, setLoading] = useState(false);
 
   const configuration = new Configuration({
@@ -95,10 +117,9 @@ function App() {
   });
   const openai = new OpenAIApi(configuration);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (index) => {
     setLoading(true);
-    const prompt = inputTexts.join(" "); // concatenate input texts into a single string
+    const prompt = inputTexts[index]; // get prompt for the input at the given index
     openai
       .createCompletion({
         model: "text-davinci-002",
@@ -109,7 +130,11 @@ function App() {
       .then((res) => {
         if (res.status === 200) {
           setLoading(false);
-          setResponse(res?.data?.choices[0]?.text);
+          setResponses((prevResponses) =>
+            prevResponses.map((response, i) =>
+              i === index ? res?.data?.choices[0]?.text : response
+            )
+          );
         }
       })
       .catch((err) => {
@@ -129,53 +154,43 @@ function App() {
         <div className="text_form">
           <form>
             {/* Add multiple input textboxes */}
-            <label>Enter text:</label>
-            <br />
-            <textarea
-              rows={3}
-              cols={80}
-              placeholder="Describe what kind of "
-              value={inputTexts[0]}
-              onChange={(e) =>
-                setInputTexts([e.target.value, inputTexts[1], inputTexts[2]])
-              }
-            />
-            <br />
-            <textarea
-              rows={3}
-              cols={80}
-              placeholder="Text 2"
-              value={inputTexts[1]}
-              onChange={(e) =>
-                setInputTexts([inputTexts[0], e.target.value, inputTexts[2]])
-              }
-            />
-            <br />
-            <textarea
-              rows={3}
-              cols={80}
-              placeholder="Text 3"
-              value={inputTexts[2]}
-              onChange={(e) =>
-                setInputTexts([inputTexts[0], inputTexts[1], e.target.value])
-              }
-            />
+            {[0, 1, 2].map((i) => (
+              <div key={i}>
+                <label>Enter text:</label>
+                <br />
+                <textarea
+                  rows={3}
+                  cols={80}
+                  placeholder={`Input ${i + 1}`}
+                  value={inputTexts[i]}
+                  onChange={(e) => {
+                    const newInputTexts = [...inputTexts];
+                    newInputTexts[i] = e.target.value;
+                    setInputTexts(newInputTexts);
+                  }}
+                />
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => handleSubmit(i)}
+                    disabled={loading}
+                  >
+                    {loading ? "Loading..." : "Submit"}
+                  </button>
+                </div>
+                <div className="response_text">
+                  <label>Response from ChatGPT</label>
+                  <textarea
+                    placeholder={`Response ${i + 1}`}
+                    cols={80}
+                    rows={4}
+                    value={responses[i]}
+                    readOnly
+                  />
+                </div>
+              </div>
+            ))}
           </form>
-        </div>
-        <div>
-          <button type="button" onClick={handleSubmit}>
-            {loading ? "Loading..." : "Submit"}
-          </button>
-        </div>
-        <div className="response_text">
-          <label>Response from ChatGPT</label>
-          <textarea
-            placeholder="ChatGPT response"
-            cols={80}
-            rows={14}
-            value={response}
-            readOnly
-          />
         </div>
       </div>
     </div>
