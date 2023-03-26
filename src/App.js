a64a1eb480c5e61f4f215cf878eb11de7013e2a4
Charles <charlespun6@gmail.com>
2023-03-25 22:42:44 -0400

    font based on category

--- src/App.js
+++ src/App.js
@@ -11,103 +11,120 @@ function App() {
   const [loading, setLoading] = useState(false);
   const responseRefs = [createRef(), createRef(), createRef()]; //contains response text
 
-  const REACT_APP_OPENAI_API_KEY = "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";
+  const REACT_APP_OPENAI_API_KEY = "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";
 
   const configuration = new Configuration({
     apiKey: REACT_APP_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
-  function generateFontCategory(categoryprompt) {
-    const [category, setCategory] = useState("");
-    async function fetchData() {
-      try {
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ prompt: prompt })
-        };
-        const category = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', requestOptions);
-        const data = await category.json();
-        setCategory(data.choices[0].text);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  
-    fetchData();
-
-    return category;
-    /* const requestBody = {
-      prompt: categoryprompt,
-      max_tokens: 100, // number of tokens to generate in the response
-      temperature: 0.5, // controls the creativity of the response
-      n: 1, // number of responses to generate
-      stop: '\n', // stop generating response after reaching this string
-    }; */
-
+  function getFontCategory(categoryprompt, originalprompt, index) {
+    openai
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: categoryprompt,
+        temperature: 0.6,
+        max_tokens: 100,
+      }).then((res) => {
+        console.log(res.data.choices[0].text);
+        const category = res.data.choices[0].text;
+        const finalprompt = "I want " + category + " fonts that will evoke " + originalprompt + ". Provide me links to corresponding fonts in Google Fonts.";
+        openai
+          .createCompletion({
+            model: "text-davinci-003",
+            prompt: finalprompt,
+            temperature: 0.6,
+            max_tokens: 100,
+          }).then((res) => {
+            if (res.status === 200) {
+              console.log(res.data.choices[0].text);
+              setLoading(false);
+              setResponses((prevResponses) =>
+                prevResponses.map((response, i) =>
+                  i === index
+                    ? index === 1
+                      ? "Generating image..."
+                      : res?.data?.choices[0]?.text
+                    : response
+                )
+              );
+            }
+          })})
+      .catch((err) => {
+        console.log(err, "An error occurred");
+      });
+      //console.log(category);
+    /* console.log(category.data.choices[0].text);
+    return category.data.choices[0].text; */
   }
 
   const handleSubmit = (index) => {
     setLoading(true);
     let prompt = inputTexts[index]; // get prompt for the input at the given index
-    if (index === 0) {
-      prompt = "I want colours that will evoke " + prompt+".";
-    }
     if (index === 2) {
+      prompt = "Given the font categories: Serif, Sans-serif, Slab serif, Script, Handwritten, and Decorative. Pick the category that will evoke"
+      + prompt + "without explanation or punctuation.";
+      getFontCategory(prompt, inputTexts[index],index);
       prompt = "I want fonts that will evoke " + prompt+". Provide me links to corresponding fonts in Google Fonts.";
     }
-
-    openai
-      .createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        temperature: 0.6,
-        max_tokens: 100,
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          setLoading(false);
-          setResponses((prevResponses) =>
-            prevResponses.map((response, i) =>
-              i === index
-                ? index === 1
-                  ? "Generating image..."
-                  : res?.data?.choices[0]?.text
-                : response
-            )
-          );
-          if (index === 1) {
-            axios
-              .post("https://api.openai.com/v1/images/generations", {
-                model: "image-alpha-001",
-                prompt: `${inputTexts[0]} ${inputTexts[1]}`,
-                size: "512x512",
-                response_format: "url",
-              }, {
-                headers: {
-                  "Authorization": `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-                  "Content-Type": "application/json",
-                },
-              })
-              .then((res) => {
-                if (res.status === 200) {
-                  setResponses((prevResponses) =>
-                    prevResponses.map((response, i) =>
-                      i === index ? res?.data?.data[0]?.url : response
-                    )
-                  );
-                }
-              })
-              .catch((err) => {
-                console.log(err, "An error occurred");
-              });
+    else {
+      if (index === 0) {
+        prompt = "I want colours that will evoke " + prompt+".";
+      }
+      if (index === 2) {
+      }
+  
+      openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt,
+          temperature: 0.6,
+          max_tokens: 100,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            setLoading(false);
+            setResponses((prevResponses) =>
+              prevResponses.map((response, i) =>
+                i === index
+                  ? index === 1
+                    ? "Generating image..."
+                    : res?.data?.choices[0]?.text
+                  : response
+              )
+            );
+            if (index === 1) {
+              axios
+                .post("https://api.openai.com/v1/images/generations", {
+                  model: "image-alpha-001",
+                  prompt: `${inputTexts[0]} ${inputTexts[1]}`,
+                  size: "512x512",
+                  response_format: "url",
+                }, {
+                  headers: {
+                    "Authorization": `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+                    "Content-Type": "application/json",
+                  },
+                })
+                .then((res) => {
+                  if (res.status === 200) {
+                    setResponses((prevResponses) =>
+                      prevResponses.map((response, i) =>
+                        i === index ? res?.data?.data[0]?.url : response
+                      )
+                    );
+                  }
+                })
+                .catch((err) => {
+                  console.log(err, "An error occurred");
+                });
+            }
           }
-        }
-      })
-      .catch((err) => {
-        console.log(err, "An error occurred");
-      });
+        })
+        .catch((err) => {
+          console.log(err, "An error occurred");
+        });  
+    }
   };
 
   // function to sanitize HTML
